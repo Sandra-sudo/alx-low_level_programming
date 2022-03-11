547be6341faf93d20b32ac1933e7d00bdb44801f
Sandra-sudo <Sandraopara54@gmail.com>
2022-03-11 06:33:53 -0800

    My commit message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+ /**
+ * main - Entry point
+ *
+ * Return: Always o (Success)
+ */
+  init main(void)
+  { 
+           Printf("with proper grammer,but the outcome is a peice of art, \n");
+           return (0);
+  }
