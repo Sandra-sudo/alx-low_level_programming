ea510397dff744bca2023bf18df11464304a03d8
Sandra-sudo <sandraopara54@gmail.com>
2022-03-15 03:32:28 +0100

    Add files via upload

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
- #include <stdio.h>
- /**
- * main - Entry point
- *
- * Return: Always o (Success)
+
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
  */
-  init main(void)
-  { 
-           Printf("with proper grammer,but the outcome is a peice of art, \n");
-           return (0);
-  }
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
