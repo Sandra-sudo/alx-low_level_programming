7388b2faacc296f21ed3204f042a7fa1784e0de4
Sandra-sudo <sandraopara54@gmail.com>
2022-04-06 05:12:59 +0100

    Add files via upload

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,41 +1,39 @@
-#include<stdlib.h>
-#include<stdio.h>
-
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * alloc_grid - a function ...
- * @width: the chaine
- * @height:
- *
- * Return: 1 or 0
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2 dim. array
  */
-
 int **alloc_grid(int width, int height)
 {
-	int **tableau2d;
+	int **grid;
 	int i, j;
 
-	if (width == 0 || height == 0)
-		return (NULL);
-	tableau2d = malloc(height * sizeof(int *));
-
-	if (tableau2d == NULL)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	for (i = 0; i < height; ++i)
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
 	{
-		tableau2d[i] = malloc(width * sizeof(int));
-		if (tableau2d[i] == NULL)
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
 		{
-			for (; i >= 0; i--)
-				free(tableau2d[i]);
-			free(tableau2d);
+			while (i >= 0)
+				free(grid[i--]);
+			free(grid);
 			return (NULL);
 		}
+
+
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
 
-	for (i = 0; i < height; ++i)
-		for (j = 0; j < width; j++)
-			tableau2d[i][j] = 0;
-
-	return (tableau2d);
+	return (grid);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,19 +1,22 @@
-#include<stdlib.h>
-#include<stdio.h>
-
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * free_grid - a function ...
- * @grid: the matrice
- * @height: the number
- *
- * Return: 1 or 0
+ * free_grid - use free function on previous grid
+ * @grid: 2 dim. array to free
+ * @height: input height (size)
+ * Return: free'd grid
  */
-
 void free_grid(int **grid, int height)
 {
-	int i = 0;
+	int i;
 
-	for (i = 0; i < height; i++)
-		free(grid[i]);
-	free(grid);
+	if (grid != NULL || height != 0)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+	}
 }
